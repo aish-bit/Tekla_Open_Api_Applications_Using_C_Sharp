54e4575bd09cb4bad1eadc0777726426533093c7
aish-bit <84989206+aish-bit@users.noreply.github.com>
2022-08-30 23:12:47 +0530

    Add files via upload

---
+++ Create_Beam/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿
+namespace Create_Beam
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.MidnightBlue;
+            this.button1.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(114, 80);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(207, 92);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Create Beam";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PeachPuff;
+            this.ClientSize = new System.Drawing.Size(470, 256);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "InputForm";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Create_Beam/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+//using System.Collections.Generic;
+//using System.ComponentModel;
+//using System.Data;
+//using System.Drawing;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tekla.Structures.Model;
+using Tekla.Structures.Geometry3d;
+
+
+
+namespace Create_Beam
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool bFlag = false;
+            
+            //create a model instance and chekc connection status
+            Model model = new Model();
+
+            if (!(model.GetConnectionStatus()))
+            {
+                MessageBox.Show("Tekla structures not connected");
+                return;
+            }
+
+
+            //creating start and end point of beam
+            Point point = new Point(0, 0, 0);  //we will assign it to start point
+            Point point2 = new Point(5000, 0, 0); //we will assign it to end point
+
+            //To create beam create object of a beam class
+            //default constructor:  Initializes a new instance of the Beam class. The start and end points are in the origin.
+            Beam beam = new Beam();
+
+            //Initialise non static characteristics of Beam class to create it
+            beam.StartPoint = point; //Gets or sets the start point of the beam.
+                                     
+            beam.EndPoint = point2;  //Gets or sets the end point of the beam.
+
+            beam.Profile.ProfileString = "CFRHS150*5";
+
+            beam.Finish = "PAINT";  //Gets or sets the finish of the part. 
+
+            beam.StartPointOffset = new Offset(); //Gets or sets the beam's start point offset.
+                                                  
+            beam.EndPointOffset = new Offset(); //Gets or sets the beam's end point offset. 
+
+            beam.Material.MaterialString = "E295";
+
+            beam.Class = "4";
+
+
+            //Inserts the beam into the model database. All the attributes/characteristics must be set. 
+            bFlag = beam.Insert();
+
+            if (bFlag == true)
+            {
+                model.CommitChanges();
+            }
+
+
+        }
+    }
+}
