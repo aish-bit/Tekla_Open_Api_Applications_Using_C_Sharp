94c92b84e83d34cf3f58e1c8bb578632a9b22982
aish-bit <84989206+aish-bit@users.noreply.github.com>
2022-08-30 23:18:06 +0530

    Add files via upload

---
+++ Hide_Part_Assignment_2/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿
+namespace Hide_Part_Assignment_2
+{
+    partial class frm_User_Input_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBox_PartName = new System.Windows.Forms.TextBox();
+            this.btn_Show_Part = new System.Windows.Forms.Button();
+            this.btn_Insert_Part = new System.Windows.Forms.Button();
+            this.lbl_FormTittle = new System.Windows.Forms.Label();
+            this.cbx_ModelType = new System.Windows.Forms.ComboBox();
+            this.lbl_PartType = new System.Windows.Forms.Label();
+            this.lbl_PartName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtBox_PartName
+            // 
+            this.txtBox_PartName.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBox_PartName.Location = new System.Drawing.Point(212, 131);
+            this.txtBox_PartName.Name = "txtBox_PartName";
+            this.txtBox_PartName.Size = new System.Drawing.Size(259, 30);
+            this.txtBox_PartName.TabIndex = 0;
+            this.txtBox_PartName.TextChanged += new System.EventHandler(this.txtBox_PartName_TextChanged);
+            // 
+            // btn_Show_Part
+            // 
+            this.btn_Show_Part.BackColor = System.Drawing.Color.MidnightBlue;
+            this.btn_Show_Part.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Show_Part.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn_Show_Part.Location = new System.Drawing.Point(295, 197);
+            this.btn_Show_Part.Name = "btn_Show_Part";
+            this.btn_Show_Part.Size = new System.Drawing.Size(176, 39);
+            this.btn_Show_Part.TabIndex = 1;
+            this.btn_Show_Part.Text = "Show Part";
+            this.btn_Show_Part.UseVisualStyleBackColor = false;
+            this.btn_Show_Part.Click += new System.EventHandler(this.btn_Show_Part_Click);
+            // 
+            // btn_Insert_Part
+            // 
+            this.btn_Insert_Part.BackColor = System.Drawing.Color.MidnightBlue;
+            this.btn_Insert_Part.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Insert_Part.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn_Insert_Part.Location = new System.Drawing.Point(50, 197);
+            this.btn_Insert_Part.Name = "btn_Insert_Part";
+            this.btn_Insert_Part.Size = new System.Drawing.Size(176, 39);
+            this.btn_Insert_Part.TabIndex = 3;
+            this.btn_Insert_Part.Text = "Insert Part";
+            this.btn_Insert_Part.UseVisualStyleBackColor = false;
+            this.btn_Insert_Part.Click += new System.EventHandler(this.btn_Insert_Part_Click);
+            // 
+            // lbl_FormTittle
+            // 
+            this.lbl_FormTittle.AutoSize = true;
+            this.lbl_FormTittle.Font = new System.Drawing.Font("Cambria", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_FormTittle.Location = new System.Drawing.Point(165, 9);
+            this.lbl_FormTittle.Name = "lbl_FormTittle";
+            this.lbl_FormTittle.Size = new System.Drawing.Size(188, 25);
+            this.lbl_FormTittle.TabIndex = 4;
+            this.lbl_FormTittle.Text = "Assignment No - 2";
+            this.lbl_FormTittle.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // cbx_ModelType
+            // 
+            this.cbx_ModelType.DropDownWidth = 121;
+            this.cbx_ModelType.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbx_ModelType.FormattingEnabled = true;
+            this.cbx_ModelType.Items.AddRange(new object[] {
+            "I-Beam",
+            "Hollow-Pipe",
+            "Column-Welded Box Profile",
+            "Polybeam-L Angle",
+            "Contour Plate"});
+            this.cbx_ModelType.Location = new System.Drawing.Point(212, 69);
+            this.cbx_ModelType.Name = "cbx_ModelType";
+            this.cbx_ModelType.Size = new System.Drawing.Size(259, 30);
+            this.cbx_ModelType.TabIndex = 5;
+            this.cbx_ModelType.SelectedIndexChanged += new System.EventHandler(this.cbx_ModelType_SelectedIndexChanged);
+            // 
+            // lbl_PartType
+            // 
+            this.lbl_PartType.AutoSize = true;
+            this.lbl_PartType.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_PartType.Location = new System.Drawing.Point(46, 77);
+            this.lbl_PartType.Name = "lbl_PartType";
+            this.lbl_PartType.Size = new System.Drawing.Size(139, 22);
+            this.lbl_PartType.TabIndex = 6;
+            this.lbl_PartType.Text = "Select Part Type";
+            // 
+            // lbl_PartName
+            // 
+            this.lbl_PartName.AutoSize = true;
+            this.lbl_PartName.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_PartName.Location = new System.Drawing.Point(46, 134);
+            this.lbl_PartName.Name = "lbl_PartName";
+            this.lbl_PartName.Size = new System.Drawing.Size(144, 22);
+            this.lbl_PartName.TabIndex = 7;
+            this.lbl_PartName.Text = "Enter Part Name";
+            this.lbl_PartName.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // frm_User_Input_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PeachPuff;
+            this.ClientSize = new System.Drawing.Size(555, 281);
+            this.Controls.Add(this.lbl_PartName);
+            this.Controls.Add(this.lbl_PartType);
+            this.Controls.Add(this.cbx_ModelType);
+            this.Controls.Add(this.lbl_FormTittle);
+            this.Controls.Add(this.btn_Insert_Part);
+            this.Controls.Add(this.btn_Show_Part);
+            this.Controls.Add(this.txtBox_PartName);
+            this.Name = "frm_User_Input_Form";
+            this.Text = "User_Input_Form";
+            this.Load += new System.EventHandler(this.frm_User_Input_Form_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBox_PartName;
+        private System.Windows.Forms.Button btn_Show_Part;
+        private System.Windows.Forms.Button btn_Insert_Part;
+        private System.Windows.Forms.Label lbl_FormTittle;
+        private System.Windows.Forms.ComboBox cbx_ModelType;
+        private System.Windows.Forms.Label lbl_PartType;
+        private System.Windows.Forms.Label lbl_PartName;
+    }
+}
+
---
+++ Hide_Part_Assignment_2/Form1.cs
@@ -0,0 +1,331 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+//using System.ComponentModel;
+//using System.Data;
+//using System.Drawing;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tekla.Structures.Model;
+using Tekla.Structures.Geometry3d;
+using Tekla.Structures.Model.Operations;
+
+
+namespace Hide_Part_Assignment_2
+{
+    public partial class frm_User_Input_Form : Form
+    {
+        private Model modelobj;
+        private Tekla.Structures.Model.UI.Picker pickerobj;
+        private Hashtable allModelObject;
+        public frm_User_Input_Form()
+        {
+            modelobj = new Model();  //create a model instance 
+            pickerobj = new Tekla.Structures.Model.UI.Picker(); //creating object of picker class to accept input from user
+            allModelObject = new Hashtable();
+            InitializeComponent();
+        }
+
+        ~frm_User_Input_Form()
+        {
+            modelobj = null;
+            pickerobj = null;
+        }
+
+        private void frm_User_Input_Form_Load(object sender, EventArgs e)
+        {
+            //txtBox_PartName.Text = "Please Enter Part name";
+        }
+
+        private void txtBox_PartName_TextChanged(object sender, EventArgs e)
+        {
+            //if(txtBox_PartName.Text == "Please Enter Part name")
+            //{
+
+            //}
+            //txtBox_PartName.Text = "";
+            //txtBox_PartName.Focus();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Insert_Part_Click(object sender, EventArgs e)
+        {
+            if (!(modelobj.GetConnectionStatus()))
+            {
+                MessageBox.Show("Tekla structures not connected");
+                return;
+            }
+
+            //Insert models in opened CAD file
+            if(cbx_ModelType.Text == "I-Beam")
+            {
+                this.insertIBeam();
+            }
+            else if (cbx_ModelType.Text == "Hollow-Pipe")
+            {
+                this.insertHollowPipe();
+            }
+            else if(cbx_ModelType.Text == "Column-Welded Box Profile")
+            {
+                this.insertWeldedBoxProfileColumn();
+            }
+            else if(cbx_ModelType.Text == "Polybeam-L Angle")
+            {
+                this.insertPolyBeamLAngle();
+            }
+            else if(cbx_ModelType.Text == "Contour Plate")
+            {
+                this.insertContourPlate();
+            }
+
+   
+
+        }
+
+        /// <summary>
+        /// /////////////////////////////////////////////////////////////////////////////
+        /// </summary>
+        /// 
+
+        private void insertIBeam()
+        {
+            //accepting start and end point of I-beam from user
+            Point startPoint = pickerobj.PickPoint("Pick start point of I-Beam");  
+            Point endPoint = pickerobj.PickPoint("Pick end point of I-Beam");  
+
+            //To create beam,create object of a beam class
+            //default constructor:  Initializes a new instance of the Beam class. The start and end points are in the origin.
+            Beam beamobj = new Beam(startPoint,endPoint);
+            
+            //Initialise non static characteristics of Beam class to create it
+
+            beamobj.Profile.ProfileString = "ISMB200"; //type of beam profile
+
+            beamobj.Name = txtBox_PartName.Text; //give name to part as per users provision
+
+            beamobj.Finish = "PAINT";  //Gets or sets the finish of the part. 
+
+            beamobj.StartPointOffset = new Offset(); //Gets or sets the beam's start point offset.
+
+            beamobj.EndPointOffset = new Offset(); //Gets or sets the beam's end point offset. 
+
+            beamobj.Material.MaterialString = "IS2062";
+
+            beamobj.Class = "3";
+
+            //Inserts the beam into the model database. All the attributes/characteristics must be set. 
+            if (beamobj.Insert())
+            {
+                modelobj.CommitChanges();
+            }
+
+            allModelObject.Add(cbx_ModelType.Text, beamobj);
+        }
+
+
+        private void insertHollowPipe()
+        {
+            //accepting start and end point of I-beam from user
+            Point startPoint = pickerobj.PickPoint("Pick start point of Hollow-pipe");
+            Point endPoint = pickerobj.PickPoint("Pick end point of Hollow-pipe");
+
+            //To create beam,create object of a beam class
+            //default constructor:  Initializes a new instance of the Beam class. The start and end points are in the origin.
+            Beam beamobj = new Beam(startPoint, endPoint);
+
+            //Initialise non static characteristics of Beam class to create it
+
+            beamobj.Profile.ProfileString = "CFCHS200*10"; //type of beam profile
+
+            beamobj.Name = txtBox_PartName.Text; //give name to part as per users provision
+
+            beamobj.Finish = "PAINT";  //Gets or sets the finish of the part. 
+
+            beamobj.StartPointOffset = new Offset(); //Gets or sets the beam's start point offset.
+
+            beamobj.EndPointOffset = new Offset(); //Gets or sets the beam's end point offset. 
+
+            beamobj.Material.MaterialString = "S355J2H";
+
+            beamobj.Class = "2";
+
+            //Inserts the beam into the model database. All the attributes/characteristics must be set. 
+            if (beamobj.Insert())
+            {
+                modelobj.CommitChanges();
+            }
+
+            allModelObject.Add(cbx_ModelType.Text, beamobj);
+        }
+
+
+        private void insertWeldedBoxProfileColumn()
+        {
+            //accepting start and end point of I-beam from user
+            Point startPoint = pickerobj.PickPoint("Pick start point of column");
+            Point endPoint = new Point();
+            endPoint.X = startPoint.X;
+            endPoint.Y = startPoint.Y;
+            endPoint.Z = 5000.0D;
+
+
+            //To create beam,create object of a beam class
+            //default constructor:  Initializes a new instance of the Beam class. The start and end points are in the origin.
+            Beam beamobj = new Beam(startPoint, endPoint);
+
+            //Initialise non static characteristics of Beam class to create it
+
+            beamobj.Profile.ProfileString = "HK300-10-15*200-25"; //type of beam profile
+
+            beamobj.Name = txtBox_PartName.Text; //give name to part as per users provision
+
+            beamobj.Finish = "PAINT";  //Gets or sets the finish of the part. 
+
+            beamobj.StartPointOffset = new Offset(); //Gets or sets the beam's start point offset.
+
+            beamobj.EndPointOffset = new Offset(); //Gets or sets the beam's end point offset. 
+
+            beamobj.Material.MaterialString = "IS2062s";
+
+            beamobj.Class = "5";
+
+            //setting column parameters
+            //Position positionobj = new Position();
+            beamobj.Position.Plane = Position.PlaneEnum.MIDDLE;
+            beamobj.Position.Rotation = Position.RotationEnum.FRONT;
+            beamobj.Position.Depth = Position.DepthEnum.MIDDLE;
+
+
+            //Inserts the beam into the model database. All the attributes/characteristics must be set. 
+            if (beamobj.Insert())
+            {
+                modelobj.CommitChanges();
+            }
+
+            allModelObject.Add(cbx_ModelType.Text, beamobj);
+            //MessageBox.Show(string.Format("string is : {0}", beamobj));
+
+        }
+
+        private void insertPolyBeamLAngle()
+        {
+            ContourPoint point = new ContourPoint(new Point(pickerobj.PickPoint("Pick first point of column")), null);
+            ContourPoint point2 = new ContourPoint(new Point(pickerobj.PickPoint("Pick second point of column")), null);
+            ContourPoint point3 = new ContourPoint(new Point(pickerobj.PickPoint("Pick third point of column")), null);
+
+            PolyBeam polyBeamobj = new PolyBeam();
+
+            polyBeamobj.AddContourPoint(point);
+            polyBeamobj.AddContourPoint(point2);
+            polyBeamobj.AddContourPoint(point3);
+
+            polyBeamobj.Profile.ProfileString = "BLL100*100*5";
+            polyBeamobj.Name = txtBox_PartName.Text;
+            polyBeamobj.Material.MaterialString = "S355J2H";
+            polyBeamobj.Finish = "PAINT";
+            polyBeamobj.Class = "13";
+
+            if(polyBeamobj.Insert())
+            {
+                modelobj.CommitChanges();
+            }
+
+            allModelObject.Add(cbx_ModelType.Text, polyBeamobj);
+
+        }
+
+
+        private void insertContourPlate()
+        {
+            ContourPoint point = new ContourPoint(new Point(pickerobj.PickPoint("Pick first point of column")), null);
+            ContourPoint point2 = new ContourPoint(new Point(pickerobj.PickPoint("Pick second point of column")), null);
+            ContourPoint point3 = new ContourPoint(new Point(pickerobj.PickPoint("Pick third point of column")), null);
+            ContourPoint point4 = new ContourPoint(new Point(pickerobj.PickPoint("Pick third point of column")), null);
+
+            ContourPlate countorplateobj = new ContourPlate();
+
+            countorplateobj.AddContourPoint(point);
+            countorplateobj.AddContourPoint(point2);
+            countorplateobj.AddContourPoint(point3);
+            countorplateobj.AddContourPoint(point4);
+
+            countorplateobj.Profile.ProfileString = "PL50";
+            countorplateobj.Name = txtBox_PartName.Text;
+            countorplateobj.Material.MaterialString = "IS2062";
+            countorplateobj.Finish = "PAINT";
+            countorplateobj.Class = "11";
+
+            if (countorplateobj.Insert())
+            {
+                modelobj.CommitChanges();
+            }
+
+            allModelObject.Add(cbx_ModelType.Text,countorplateobj);
+        }
+
+        //private void showSelectedPartOnly()
+        //{
+        //    //string sTemp = cbx_ModelType.Text;
+
+        //    //List<string> hideItemList = new List<string>();
+
+        //    //foreach(string item in cbx_ModelType.Items)
+        //    //{
+        //    //    if(txtBox_PartName.Text != sTemp)
+        //    //    {
+        //    //        hideItemList.Add(item);
+        //    //    }
+        //    //}
+
+        //    Operation.ShowOnlySelected(Operation.UnselectedModeEnum.Hidden);
+
+        //}
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cbx_ModelType_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Show_Part_Click(object sender, EventArgs e)
+        {
+            ArrayList objectsToSelect = new ArrayList();
+
+            Tekla.Structures.Model.UI.ModelObjectSelector modelobjselectorobj = new Tekla.Structures.Model.UI.ModelObjectSelector();
+
+            foreach (string partType in allModelObject.Keys)
+            {
+                if (partType == cbx_ModelType.Text)
+                {
+                    objectsToSelect.Add(allModelObject[partType]);
+                    modelobjselectorobj.Select(objectsToSelect);
+                }
+
+            }
+            
+            Operation.ShowOnlySelected(Operation.UnselectedModeEnum.Hidden);
+
+            modelobj.CommitChanges();
+        }
+    }
+}
